db013a3af2cd4dbf66c481bce33c4ec13db25d19
Nastia <nastiakopylova1@mail.ru>
2023-10-11 22:50:42 +0300

    implemented employees rendering logic at the employees page

--- src/pages/EmployeesPage/EmployeesPage.tsx
+++ src/pages/EmployeesPage/EmployeesPage.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectEmployees } from "../../store/employees-selectors";
+import EmployeeCard from "../../components/EmployeeCard/EmployeeCard";
+import './EmployeesPage.scss';
+
 
 const EmployeesPage: React.FC = () => {
+  const employees = useSelector(selectEmployees);
+
   return (
-    <div>Employees Page</div>
+    <div className="employees-page">
+      <h1>Employees Page</h1>
+      <div className="employees-list">
+        {
+          employees.map(employee => {
+            return (
+              <EmployeeCard 
+                key={employee.id}
+                name={employee.name}
+                role={employee.role}
+                phone={employee.phone} 
+              />
+            )
+          })
+        }
+      </div>
+    </div>
   )
 };
 
