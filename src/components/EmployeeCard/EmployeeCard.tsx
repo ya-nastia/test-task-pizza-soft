cc308e92a6cb78a85d48c6b67f5415aa99a51dd7
Nastia <nastiakopylova1@mail.ru>
2023-10-12 22:01:16 +0300

    implemented adaptive styles for main page

--- src/components/EmployeeCard/EmployeeCard.tsx
+++ src/components/EmployeeCard/EmployeeCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './EmployeeCard.scss';
 
 interface IEmployeeCard {
   id: number;
@@ -13,8 +14,8 @@ const EmployeeCard: React.FC<IEmployeeCard> = ({ id, name, role, phone }) => {
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
-        <p className="card-text">Role: {role}</p>
-        <p className="card-text">Phone Number: {phone}</p>
+        <p className="card-text"><b>Role:</b> <span>{role}</span></p>
+        <p className="card-text"><b>Phone Number:</b> <span>{phone}</span></p>
         <Link to={`/edit-employee/${id}`} className="btn btn-primary">Edit</Link>
       </div>
     </div>
